f02137d6756baad794903c1b9492caf99825b9f8
berbelmercado <berbelmercado@gmail.com>
2026-02-11 13:37:46 -0500

    feat: agregar main

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ def sumar_dos_numero(numero1: int, numero2: int) -> int:
     return numero1 + numero2
 
 
+def multiplicar_dos_numero(numero1: int, numero2: int) -> int:
+    return numero1 * numero2
+
+
 print(sumar_dos_numero(5, 10))
